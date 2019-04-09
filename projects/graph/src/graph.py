23a245b515ab71378509b0a65f470d6841a6be3d
jamespagedev <jamespagedev@outlook.com>
2019-04-08 22:30:07 -0700

    Graph bfs/dfs completed... but not tested

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -108,20 +108,20 @@ class Graph:
     def add_vertex(self, verNum):
         self.vertices[verNum] = set()
 
-    def add_directed_edge(self, verNum, edgeNum):
-        if verNum not in self.vertices: # Stretch, O(1)
-            raise ValueError("No %s vertex" % (verNum))
-        elif edgeNum not in self.vertices: # Stretch, O(1)
-            raise ValueError("No %s vertex" % (edgeNum))
-        self.vertices[verNum].add(edgeNum)
-
-    def add_bidirected_edge(self, ver1, ver2):
+    def add_edge(self, ver1, ver2):
         if ver1 not in self.vertices: # Stretch, O(1)
             raise ValueError("No %s vertex" % (ver1))
         elif ver2 not in self.vertices: # Stretch, O(1)
             raise ValueError("No %s vertex" % (ver2))
         self.vertices[ver1].add(ver2)
         self.vertices[ver2].add(ver1)
+
+    def add_directed_edge(self, verNum, edgeNum):
+        if verNum not in self.vertices: # Stretch, O(1)
+            raise ValueError("No %s vertex" % (verNum))
+        elif edgeNum not in self.vertices: # Stretch, O(1)
+            raise ValueError("No %s vertex" % (edgeNum))
+        self.vertices[verNum].add(edgeNum)
 
     # Part 2
     def bft_queue(self, startVer):
@@ -154,49 +154,77 @@ class Graph:
                     s.push(next_vert)
 
     # Part 3.5
-    def dft_recursion(self, starting_vert, visited=set()):
+    def dft_recursion(self, vert, visited=set()):
         # No instruction given
-        visited.add(starting_vert)
+        visited.add(vert)
 
-        for neighbor in self.vertices[starting_vert]:
-            if neighbor not in visited:
-                return self.dft_recursion(neighbor, visited)
+        for neighbor_vert in self.vertices[vert]:
+            if neighbor_vert not in visited:
+                self.dft_recursion(neighbor_vert, visited)
+
+        return visited
 
     # Part 4
-    def bfs(self, graph, startVert):
-        for v of graph.vertexes:
-            v.color = 'white'
+    def bfs(self, starting_vert_id, target_vert):
+        # Instructions from Q&A...
+        # create empty queue
+        q = Queue()
+        # create a visited set
+        visited = set()
+        # enqueue [A PATH TO] the starting vertex to the queue
+        q.enqueue([starting_vert_id])
+        # while the queue is not empty...
+        while q.len() > 0:
+            # dequeue the first [PATH] from the queue
+            q_in_check = q.dequeue()
+            # pull the last vertex from the path
+            # check if it's visited...
+            if q_in_check[-1] not in visited:
+                # mark it as visited
+                visited.add(q_in_check[-1])
+                # check if it's equal to the target vertex, if so return the path
+                if q_in_check[-1] == target_vert:
+                    return q_in_check
+                # put [A PATH TO] all of its neighbors in the back of the queue
+                for neighbor_vert in self.vertices[q_in_check[-1]]:
+                    # copy the path
+                    path = q_in_check
+                    # append the neighbor vertex to the path
+                    path.append(neighbor_vert)
+                    # enqueue the new path
+                    q.enqueue(path)
 
-        startVert.color = 'gray'
-        queue.enqueue(startVert)
-
-        while queue.len() > 0:
-            u = queue[0]  # Peek at head of queue, but do not dequeue!
-
-            for v of u.neighbors:
-                if v.color == 'white':
-                    v.color = 'gray'
-                    queue.enqueue(v)
-
-            queue.dequeue()
-            u.color = 'black'
+        return f"vert {target_vert} not found in graph"
 
     # Part 5
-    def dfs(graph):
-        for v of graph.verts:
-            v.color = 'white'
-            v.parent = None
+    def dfs(self, starting_vert_id, target_vert):
+        # Instructions from Q&A (DFS is same as BFS, but replace queue with stack)...
+            # Note: Guranteed to give you a working path (if exist), but not the shortest path
+        # create empty stack
+        s = Stack()
+        # create a visited set
+        visited = set()
+        # push [A PATH TO] the starting vertex to the stack
+        s.push([starting_vert_id])
+        # while the stack is not empty...
+        while s.len() > 0:
+            # pop the last [PATH] from the stack
+            s_in_check = s.pop()
+            # pull the last vertex from the path
+            # check if it's visited...
+            if s_in_check[-1] not in visited:
+                # mark it as visited
+                visited.add(s_in_check[-1])
+                # check if it's equal to the target vertex, if so return the path
+                if s_in_check[-1] == target_vert:
+                    return s_in_check
+                # put [A PATH TO] all of its neighbors in the back of the stack
+                for neighbor_vert in self.vertices[s_in_check[-1]]:
+                    # copy the path
+                    path = s_in_check
+                    # append the neighbor vertex to the path
+                    path.append(neighbor_vert)
+                    # enqueue the new path
+                    s.push(path)
 
-        for v of graph.verts:
-            if v.color == 'white':
-                dfs_visit(v)
-
-    def dfs_visit(v):
-        v.color = 'gray'
-
-        for neighbor of v.adjacent_nodes:
-            if neighbor.color == 'white':
-                neighbor.parent = v
-                dfs_visit(neighbor)
-
-        v.color = 'black'
+        return f"vert {target_vert} not found in graph"
